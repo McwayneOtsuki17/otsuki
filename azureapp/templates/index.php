a1ec53a75634f85a0b393dfc2777e9b6b074dd46
KyTzuki <meadowmc17@gmail.com>
2022-03-16 17:57:11 +0900

    update 1.0 index page
    added a few code lines and included my middle initial

--- azureapp/templates/index.php
+++ azureapp/templates/index.php
@@ -1,2 +1,15 @@
-<H1>Otsuki, McWayne</H1>
-<H2>Bachelor of Science in Information Technology - III</H2>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>My Simple Web App</title>
+</head>
+
+<body>
+    <h1>Otsuki, McWayne Q.</h1>
+    <h2>Bachelor of Science in Information Technology - III</h2>
+</body>
+
+</html>
