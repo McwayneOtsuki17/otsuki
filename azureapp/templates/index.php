0b9410e0fbd118335e18c93045b10a620416f1ce
KyTzuki <meadowmc17@gmail.com>
2022-03-20 16:19:51 +0900

    Added design

--- azureapp/templates/index.php
+++ azureapp/templates/index.php
@@ -1,15 +1,144 @@
+{% load static %}
 <!DOCTYPE html>
-<html lang="en">
+<html style="font-size: 16px;">
 
 <head>
-    <meta charset="UTF-8">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <title>My Simple Web App</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="utf-8">
+  <meta name="keywords" content="INTUITIVE">
+  <meta name="description" content="">
+  <meta name="page_type" content="np-template-header-footer-from-plugin">
+  <title>大槻 - HOME</title>
+  <link rel="stylesheet" href="{% static 'css/nicepage.css' %}" media="screen">
+  <link rel="stylesheet" href="{% static 'css/index.css' %}" media="screen">
+  <script class="u-script" type="text/javascript" src="{% static 'js/jquery.js' %}" defer=""></script>
+  <script class="u-script" type="text/javascript" src="{% static 'js/nicepage.js' %}" defer=""></script>
+  <meta name="generator" content="Nicepage 4.6.5, nicepage.com">
+  <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+  <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+
+
+  <script type="application/ld+json">
+    {
+      "@context": "http://schema.org",
+      "@type": "Organization",
+      "name": ""
+    }
+  </script>
+  <meta name="theme-color" content="#a68053">
+  <meta property="og:title" content="HOME">
+  <meta property="og:type" content="website">
 </head>
 
-<body>
-    <h1>Otsuki, McWayne Q.</h1>
-    <h2>Bachelor of Science in Information Technology - III</h2>
+<body data-home-page="#" data-home-page-title="HOME" class="u-body u-xl-mode">
+  <header class="u-clearfix u-header u-header" id="sec-c148">
+    <div class="u-clearfix u-sheet u-sheet-1">
+      <a href="#" class="u-border-active-palette-2-base u-border-hover-palette-1-base u-border-none u-btn u-button-style u-custom-font u-heading-font u-none u-text-active-palette-5-base u-text-grey-80 u-text-hover-palette-1-base u-btn-1">大槻<span style="font-weight: 700;">
+          <span style="font-weight: 400;"></span>
+        </span>
+      </a>
+      <nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1" data-responsive-from="XL">
+        <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px; font-weight: 700;">
+          <a class="u-button-style u-custom-active-border-color u-custom-active-color u-custom-border u-custom-border-color u-custom-borders u-custom-hover-border-color u-custom-hover-color u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-text-active-color u-custom-text-color u-custom-text-hover-color u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#" style="font-size: calc(1em + 6px); padding: 3px 14px;">
+            <svg class="u-svg-link" viewBox="0 0 24 24">
+              <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#menu-hamburger"></use>
+            </svg>
+            <svg class="u-svg-content" version="1.1" id="menu-hamburger" viewBox="0 0 16 16" x="0px" y="0px" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg">
+              <g>
+                <rect y="1" width="16" height="2"></rect>
+                <rect y="7" width="16" height="2"></rect>
+                <rect y="13" width="16" height="2"></rect>
+              </g>
+            </svg>
+          </a>
+        </div>
+        <div class="u-custom-menu u-nav-container">
+          <ul class="u-nav u-spacing-0 u-unstyled u-nav-1">
+            <li class="u-nav-item"><a class="u-border-2 u-border-active-palette-1-dark-1 u-border-grey-30 u-border-hover-grey-30 u-border-no-bottom u-border-no-left u-border-no-top u-button-style u-nav-link u-text-active-palette-1-base u-text-grey-90 u-text-hover-palette-3-base" href="#" style="padding: 0px 20px;">HOME</a>
+            </li>
+            <li class="u-nav-item"><a class="u-border-2 u-border-active-palette-1-dark-1 u-border-grey-30 u-border-hover-grey-30 u-border-no-bottom u-border-no-left u-border-no-top u-button-style u-nav-link u-text-active-palette-1-base u-text-grey-90 u-text-hover-palette-3-base" style="padding: 0px 28px 0px 20px;">ABOUT</a>
+            </li>
+          </ul>
+        </div>
+        <div class="u-custom-menu u-nav-container-collapse">
+          <div class="u-container-style u-inner-container-layout u-sidenav u-white">
+            <div class="u-inner-container-layout u-sidenav-overflow">
+              <div class="u-menu-close"></div>
+              <ul class="u-align-center u-custom-font u-heading-font u-nav u-popupmenu-items u-spacing-0 u-text-active-palette-1-base u-text-hover-palette-5-base u-unstyled u-nav-2">
+                <li class="u-nav-item"><a class="u-button-style u-nav-link" href="index.html" style="padding: 26px 20px;">HOME</a>
+                </li>
+                <li class="u-nav-item"><a class="u-button-style u-nav-link" style="padding: 26px 20px;">ABOUT</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="u-grey-90 u-menu-overlay u-opacity u-opacity-85"></div>
+        </div>
+      </nav>
+    </div>
+  </header>
+  <section class="u-align-center u-clearfix u-image u-shading u-section-1" src="" data-image-width="2208" data-image-height="1656" id="sec-c761">
+    <div class="u-clearfix u-sheet u-sheet-1">
+      <h1 class="u-hover-feature u-text u-text-custom-color-2 u-title u-text-1" style="font-size: 70px;">大槻, ​マクウェイン</h1>
+      <h1 class="u-custom-font u-text u-text-default u-text-font u-title u-text-2" style="font-size: 70px;" data-animation-name="customAnimationIn" data-animation-duration="2500" data-animation-direction="">OTSUKI​, MCWAYNE</h1>
+      <br>
+      <h1 class="u-custom-font u-text u-text-default u-text-font u-text-3" style="font-size: 20px;" data-animation-name="customAnimationIn" data-animation-duration="2750">BACHELOR OF SCIENCE<br>IN INFORMATION TECHNOLOGY - III</h1>
+      <div class="u-align-left u-social-icons u-spacing-10 u-social-icons-1" data-animation-name="customAnimationIn" data-animation-duration="2500" data-animation-direction="">
+        <a class="u-social-url" title="facebook" href="#"><span class="u-icon u-social-facebook u-social-icon u-text-palette-3-dark-1"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style="">
+              <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-f99e"></use>
+            </svg><svg class="u-svg-content" viewBox="0 0 112 112" x="0" y="0" id="svg-f99e">
+              <circle fill="currentColor" cx="56.1" cy="56.1" r="55"></circle>
+              <path fill="#FFFFFF" d="M73.5,31.6h-9.1c-1.4,0-3.6,0.8-3.6,3.9v8.5h12.6L72,58.3H60.8v40.8H43.9V58.3h-8V43.9h8v-9.2
+            c0-6.7,3.1-17,17-17h12.5v13.9H73.5z"></path>
+            </svg></span>
+        </a>
+        <a class="u-social-url" data-type="Github" title="Github" href="#"><span class="u-icon u-social-github u-social-icon u-text-palette-3-dark-1 u-icon-2"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style="">
+              <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-2beb"></use>
+            </svg><svg class="u-svg-content" viewBox="0 0 112 112" x="0" y="0" id="svg-2beb">
+              <circle fill="currentColor" cx="56.1" cy="56.1" r="55"></circle>
+              <path fill="#FFFFFF" d="M88,51.3c0-5.5-1.9-10.2-5.3-13.7c0.6-1.3,2.3-6.5-0.5-13.5c0,0-4.2-1.4-14,5.3c-4.1-1.1-8.4-1.7-12.7-1.8
+	c-4.3,0-8.7,0.6-12.7,1.8c-9.7-6.6-14-5.3-14-5.3c-2.8,7-1,12.2-0.5,13.5C25,41.2,23,45.7,23,51.3c0,19.6,11.9,23.9,23.3,25.2
+	c-1.5,1.3-2.8,3.5-3.2,6.8c-3,1.3-10.2,3.6-14.9-4.3c0,0-2.7-4.9-7.8-5.3c0,0-5-0.1-0.4,3.1c0,0,3.3,1.6,5.6,7.5c0,0,3,9.1,17.2,6
+	c0,4.3,0.1,8.3,0.1,9.5h25.2c0-1.7,0.1-7.2,0.1-14c0-4.7-1.7-7.9-3.4-9.4C76,75.2,88,70.9,88,51.3z"></path>
+            </svg></span>
+        </a>
+        <a class="u-social-url" title="instagram" href="#"><span class="u-icon u-social-icon u-social-instagram u-text-palette-3-dark-1"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style="">
+              <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-73c3"></use>
+            </svg><svg class="u-svg-content" viewBox="0 0 112 112" x="0" y="0" id="svg-73c3">
+              <circle fill="currentColor" cx="56.1" cy="56.1" r="55"></circle>
+              <path fill="#FFFFFF" d="M55.9,38.2c-9.9,0-17.9,8-17.9,17.9C38,66,46,74,55.9,74c9.9,0,17.9-8,17.9-17.9C73.8,46.2,65.8,38.2,55.9,38.2
+            z M55.9,66.4c-5.7,0-10.3-4.6-10.3-10.3c-0.1-5.7,4.6-10.3,10.3-10.3c5.7,0,10.3,4.6,10.3,10.3C66.2,61.8,61.6,66.4,55.9,66.4z"></path>
+              <path fill="#FFFFFF" d="M74.3,33.5c-2.3,0-4.2,1.9-4.2,4.2s1.9,4.2,4.2,4.2s4.2-1.9,4.2-4.2S76.6,33.5,74.3,33.5z"></path>
+              <path fill="#FFFFFF" d="M73.1,21.3H38.6c-9.7,0-17.5,7.9-17.5,17.5v34.5c0,9.7,7.9,17.6,17.5,17.6h34.5c9.7,0,17.5-7.9,17.5-17.5V38.8
+            C90.6,29.1,82.7,21.3,73.1,21.3z M83,73.3c0,5.5-4.5,9.9-9.9,9.9H38.6c-5.5,0-9.9-4.5-9.9-9.9V38.8c0-5.5,4.5-9.9,9.9-9.9h34.5
+            c5.5,0,9.9,4.5,9.9,9.9V73.3z"></path>
+            </svg></span>
+        </a>
+        <a class="u-social-url" target="_blank" data-type="Email" title="Email" href="mailto:meadowmc17@gmail.com"><span class="u-icon u-social-email u-social-icon u-text-palette-3-dark-1 u-icon-4"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style="">
+              <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-4da4"></use>
+            </svg><svg class="u-svg-content" viewBox="0 0 112 112" x="0" y="0" id="svg-4da4">
+              <circle fill="currentColor" cx="56.1" cy="56.1" r="55"></circle>
+              <path id="path3864" fill="#FFFFFF" d="M27.2,28h57.6c4,0,7.2,3.2,7.2,7.2l0,0v42.7c0,3.9-3.2,7.2-7.2,7.2l0,0H27.2
+	c-4,0-7.2-3.2-7.2-7.2V35.2C20,31.1,23.2,28,27.2,28 M56,52.9l28.8-17.8H27.2L56,52.9 M27.2,77.7h57.6V43.5L56,61.3L27.2,43.5V77.7z
+	"></path>
+            </svg></span>
+        </a>
+      </div>
+    </div>
+  </section>
+  <footer class="u-clearfix u-footer u-grey-80" id="sec-b6cf">
+    <div class="u-clearfix u-sheet u-sheet-1"></div>
+  </footer>
+  <!-- <section class="u-backlink u-clearfix u-grey-80">
+      <a class="u-link" href="https://nicepage.com/website-templates" target="_blank">
+        <span>Website Templates</span>
+      </a>
+      <p class="u-text">
+        <span>created with</span>
+      </p>
+      <a class="u-link" href="" target="_blank">
+        <span>Website Builder Software</span>
+      </a>. 
+    </section> -->
 </body>
-
 </html>
